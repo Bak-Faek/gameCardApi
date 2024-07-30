060aef453a482743e02d7c5db97cfd7b9b4d3046
Patrick Hoang <hoang.patrick@outlook.com>
2024-07-30 17:18:51 +0200

    connexion bdd ok

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,21 +1,15 @@
 import { DataSourceOptions } from 'typeorm';
-
 import 'dotenv/config';
 
 export const databaseConfig: DataSourceOptions = {
   type: 'mysql',
-
   host: process.env.DATABASE_HOST,
-
-  port: parseInt(process.env.DATABASE_PORT || '3000'),
-
+  port: parseInt(process.env.DATABASE_PORT || '3306'),
   username: process.env.DATABASE_USERNAME,
-
   password: process.env.DATABASE_PASSWORD,
-
   database: process.env.DATABASE_DB,
-
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-
+  migrations: ['src/migration/**/*.ts'],
+  migrationsTableName: 'migrations',
   synchronize: false,
 };
