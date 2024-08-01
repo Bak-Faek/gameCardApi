523005c0b8d6e5aac81698b138998e0e1e1a2a92
mohamaddayem <u@example.com>
2024-08-01 03:59:16 +0300

    ready

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Put,
+  Delete,
+  Param,
+} from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+import { userService } from './user.service';
+import { User } from './user.entity/user.entity';
+
+@Controller('/user')
+export class UserController {
+  constructor(private service: userService) {}
+
+  @Get()
+  getAll() {
+    return this.service.getusers();
+  }
+
+  @Get(':id')
+  get(@Param() params) {
+    return this.service.getuser(params.id);
+  }
+
+  @Post()
+  create(@Body() user: User) {
+    return this.service.saveuser(user);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() user: User): Promise<any> {
+    return this.service.modifyuser(+id, user);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param() params) {
+    this.service.deleteuser(params.id);
+    return;
+  }
+}
